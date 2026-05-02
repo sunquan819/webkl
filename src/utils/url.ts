c6da57837e67965614b680c8c1fad61485bc5104
sunquan819 <103480187+sunquan819@users.noreply.github.com>
2026-05-02 13:05:18 +0800

    clean: remove unused url.ts

--- src/utils/url.ts
+++
@@ -1,7 +0,0 @@
-export const BASE_PATH = '/webkl';
-
-export function url(path: string): string {
-  if (path.startsWith(BASE_PATH)) return path;
-  if (path === '/') return BASE_PATH + '/';
-  return BASE_PATH + path;
-}
